51c4a31ad082a9f764966325048d8990f1cffe10
zamanmudassir200 <zamanmudassir200@gmail.com>
2025-03-28 00:13:15 +0500

    url changed from local to deployed vercel url

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,3 +1,3 @@
 // const url = `http://localhost:3000`;
-const url = `https://ecommerce-fullstack-design-pied.vercel.app`;
+const url = `https://ecommerce-fullstack-design-theta.vercel.app`;
 export default url;
