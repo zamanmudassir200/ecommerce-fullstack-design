b4022653357ecfb62bed33c6a616973ab3ec9a86
zamanmudassir200 <zamanmudassir200@gmail.com>
2025-04-12 12:17:25 +0500

    url changed

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,3 +1,3 @@
 // const url = `http://localhost:3000`;
-const url = `https://ecommerce-fullstack-design-zsgh.vercel.app/`;
+const url = `https://ecommerce-fullstack-design-zsgh.vercel.app`;
 export default url;
